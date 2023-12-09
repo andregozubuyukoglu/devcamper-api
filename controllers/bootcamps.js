66484ac50942c73624f7042b2d840cd86bea760b
Andre <andregozubuyuk@gmail.com>
2023-12-09 22:30:51 +0300

    Updated files

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse")
 const Bootcamp = require("../models/Bootcamp")
 const asyncHandler = require("../middleware/async")
+const geocoder = require("../utils/geocoder")
 
 // @description     GET all bootcamps
 // @route           GET /api/v1/bootcamps
@@ -71,4 +72,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: {} })
+})
+
+// @description     Get bootcamps within a radius
+// @route           GET /api/v1/bootcamp/radius/:zipcode/:distance
+// @access          Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode)
+  const lat = loc[0].latitude
+  const lag = loc[0].longitude
+
+  //Calc radius using radians
+  //Divide dist by radius of Earth
+  const radius = distance / 3963
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  })
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  })
 })
