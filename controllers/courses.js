c5d124f5863073fe4e7c783d97f0e4681d0e135c
Andre <andregozubuyuk@gmail.com>
2023-12-16 23:38:45 +0300

    Updated files

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse")
 const Course = require("../models/Course")
+const Bootcamp = require("../models/Bootcamp")
 const asyncHandler = require("../middleware/async")
 
 // @description     GET courses
@@ -23,5 +24,44 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses,
+  })
+})
+
+// @description     GET single courses
+// @route           GET /api/v1/courses/:id
+// @access          Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description",
+  })
+
+  if (!course) {
+    return next(new ErrorResponse(`Course not found`), 404)
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  })
+})
+
+// @description     Add course
+// @route           GET /api/v1/bootcamps/:bootcampId/courses
+// @access          Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId
+
+  const bootcamp = await Bootcamp.findById(req.params.id.bootcampId)
+
+  if (!course) {
+    return next(new ErrorResponse(`Bootcamp not found`), 404)
+  }
+
+  const course = await Course.create(req.body)
+
+  res.status(200).json({
+    success: true,
+    data: course,
   })
 })
