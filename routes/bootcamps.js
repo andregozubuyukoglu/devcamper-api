385129281a6f1c52abf144832cd4f350180fc6b2
Andre <andregozubuyuk@gmail.com>
2023-12-22 23:08:01 +0300

    Updated files

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -12,12 +12,10 @@ const {
 const Bootcamp = require("../models/Bootcamp")
 const advancedResults = require("../middleware/advancedResults")
 
-// Include other resource routers
 const courseRouter = require("./courses")
 
 const router = express.Router()
 
-// Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter)
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius)
