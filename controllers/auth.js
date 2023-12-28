d6e2f1949edee37475a64a0b1ff2d0e4252719a5
Andre <andregozubuyuk@gmail.com>
2023-12-28 19:59:51 +0300

    Updated files

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,6 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   })
 
-  // Create token
   const token = user.getSignedJwtToken()
 
   res.status(200).json({ success: true, token })
