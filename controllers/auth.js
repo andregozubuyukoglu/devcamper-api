2eaf34474899c6a11740a8fcc684ccc8410b6749
Andre <andregozubuyuk@gmail.com>
2024-01-02 22:00:08 +0300

    Updated files

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,6 @@ const Bootcamp = require("../models/Bootcamp")
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body
 
-  // Create user
   const user = await User.create({
     name,
     email,
@@ -29,26 +28,22 @@ exports.register = asyncHandler(async (req, res, next) => {
 exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body
 
-  // Validate emil & password
   if (!email || !password) {
     return next(new ErrorResponse("Please provide an email and password", 400))
   }
 
-  // Check for user
   const user = await User.findOne({ email }).select("+password")
 
   if (!user) {
     return next(new ErrorResponse("Invalid credentials", 401))
   }
 
-  // Check if password matches
   const isMatch = await user.matchPassword(password)
 
   if (!isMatch) {
     return next(new ErrorResponse("Invalid credentials", 401))
   }
 
-  // Create token
   const token = user.getSignedJwtToken()
 
   res.status(200).json({ success: true, token })
